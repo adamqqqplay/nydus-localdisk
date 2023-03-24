0fab78eb904a25c35c1c058b514043de3bd4a2ef
Qinqi Qu <quqinqi@linux.alibaba.com>
2023-03-24 15:10:15 +0800

    cmd: use urfave/cli to build the command line
    Signed-off-by: Qinqi Qu <quqinqi@linux.alibaba.com>

--- cmd/nydus-localdisk/nydus-localdisk.go
+++ cmd/nydus-localdisk/nydus-localdisk.go
@@ -8,49 +8,21 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
 
 	"github.com/adamqqqplay/nydus-localdisk/pkg/generator"
-
 	log "github.com/sirupsen/logrus"
+	"github.com/urfave/cli/v2"
 )
 
 func printWelcome() {
 	var str = `
-    Nydus localdisk generator (golang)
-    Usage: nydus-localdisk $IMAGE_PATH $TARGET_DIR
+    Welcome to use nydus-localdisk!
     License: Apache-2.0
     `
 
 	fmt.Println(str)
-}
-
-const defaultTargetDir string = "./workdir"
-
-func readCommandArgs() (imagePath string, targetDir string, err error) {
-	var args = os.Args
-	var argNum = len(args)
-
-	if argNum < 2 {
-		var str = fmt.Sprintf("Argument $IMAGE_PATH required, such as: %s localhost:5000/ubuntu-nydus", args[0])
-		err = errors.New(str)
-		return "", "", err
-	} else if argNum >= 4 {
-		var str = fmt.Sprintf("%d arguments are too many for this program, up to 2 arguments required", argNum-1)
-		err = errors.New(str)
-		return "", "", err
-	} else if argNum == 3 {
-		imagePath = args[1]
-		targetDir = args[2]
-	} else if argNum == 2 {
-		log.Infof("$TARGET_DIR not provided, use default dir: %s", defaultTargetDir)
-		imagePath = args[1]
-		targetDir = defaultTargetDir
-	}
-
-	return imagePath, targetDir, nil
 }
 
 func main() {
@@ -59,10 +31,41 @@ func main() {
 	})
 
 	printWelcome()
-	var imagePath, workDir, err = readCommandArgs()
-	if err != nil {
-		log.Fatalln(err)
+
+	app := &cli.App{
+		Name:  "nydus-localdisk",
+		Usage: "provide Nydus localdisk backend support",
+		Commands: []*cli.Command{
+			{
+				Name:  "convert",
+				Usage: "converts a Nydus image from the source registry to a gpt disk image file in localdisk format.",
+				Flags: []cli.Flag{
+					&cli.StringFlag{
+						Name:     "source",
+						Usage:    "source image reference (example: localhost:5000/ubuntu-nydus)",
+						Required: true,
+						EnvVars:  []string{"SOURCE"},
+					},
+					&cli.StringFlag{
+						Name:     "target",
+						Value:    "./workdir",
+						Usage:    "target localdisk dir",
+						Required: false,
+						EnvVars:  []string{"TARGET"},
+					},
+				},
+				Action: func(cCtx *cli.Context) error {
+					var imagePath = cCtx.String("source")
+					var workDir = cCtx.String("target")
+					generator.ConvertImage(imagePath, workDir, false)
+
+					return nil
+				},
+			},
+		},
 	}
 
-	generator.ConvertImage(imagePath, workDir, false)
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
 }
